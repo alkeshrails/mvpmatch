a5ae83e5cb221cc21dd5ff6fe0ed714cb2ff30e1
alkeshrails <akleshrailsdev@gmail.com>
2023-01-17 18:43:12 +0530

    Add login api and routes

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router()
 
 const baseUrl = '/auth'
 
+router.post(baseUrl + '/login', AuthController.userLogin)
 router.post(baseUrl + '/register', AuthController.userRegistration)
 
 export default router
